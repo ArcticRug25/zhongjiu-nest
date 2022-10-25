14b30cdfaf92cdde4e0b1c6b18570d4b5d22458e
ArcticRug25 <wyw82839148@163.com>
2022-10-25 23:43:59 +0800

    feat: add user time

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,10 +1,13 @@
 import { User } from '@prisma/client'
 import { Exclude, Transform } from 'class-transformer'
-
+import dayjs from 'dayjs'
 export class UserEntity {
   @Exclude()
   password: string
-
+  @Transform(({ value }) => dayjs(value).format('YYYY-MM-DD'))
+  createAt: string
+  @Transform(({ value }) => dayjs(value).format('YYYY-MM-DD'))
+  updateAt: string
   constructor(options: Partial<User>) {
     Object.assign(this, options)
   }
