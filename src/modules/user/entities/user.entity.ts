a4cea1ecca5cd3fa3ede37fa75f4cd5e1ee6221a
ArcticRug25 <wyw82839148@163.com>
2022-10-30 21:52:23 +0800

    feat: add updateAt

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -6,6 +6,8 @@ export class UserEntity {
   password: string
   @Transform(({ value }) => dayjs(value).format('YYYY-MM-DD'))
   createAt: string
+  @Transform(({ value }) => dayjs(value).format('YYYY-MM-DD'))
+  updateAt: string
   constructor(options: Partial<User>) {
     Object.assign(this, options)
   }
